3b25603eec75442ecd45f322b719ef4a5f951dad
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2022-08-24 11:47:25 +0200

    fix: add missing export

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as iterate } from "./iterate.js";
 export { default as newEventEmitter } from "./newEventEmitter.js";
 export { default as newMutex } from "./newMutex.js";
 export { default as newRegistry } from "./newRegistry.js";
+export { default as observe } from "./observe.js";
